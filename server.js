940f5855eb3a4b40228af27008f05565e2b40d17
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-09-01 13:54:04 +0530

    server config added

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
