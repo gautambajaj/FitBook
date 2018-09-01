c3760301701e58bd8c89d4e78fbfa08c252cc164
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-09-01 15:49:13 +0530

    remove redux chrome tool on deployment

--- client/src/store.js
+++ client/src/store.js
@@ -10,9 +10,7 @@ const store = createStore(
 	rootReducer, 
 	initialState, 
 	compose(
-		// enable redux chrome devtool
-		applyMiddleware(...middleware), 
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		applyMiddleware(...middleware)
 	)
 );
 
