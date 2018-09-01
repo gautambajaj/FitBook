e33d9da6eea52cc94244081c221277fa23be042e
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-09-01 16:59:52 +0530

    Made dashboard responsive

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -5,6 +5,19 @@ const ProfileActions = () => {
 	return(
           <div className="container">
             <div className="row">
+                <div className="btn-group-vertical d-sm-none mb-4" role="group">
+                  <Link to="/edit-profile" className="btn btn-light">
+                    <i className="fas fa-user-circle text-info mr-1"></i> Edit Profile
+                  </Link>
+                  <Link to="/recipe-search" className="btn btn-light">
+                    <i className="fas fa-utensils text-info mr-1"></i>
+                    Browse Recipes
+                  </Link>
+                  <Link to="/feed" className="btn btn-light">
+                    <i className="fa fa-rss text-info mr-1"></i>
+                    View Post-Feed
+                  </Link>
+                </div>
                 <div className="btn-group d-none d-sm-block mb-4" role="group">
                   <Link to="/edit-profile" className="btn btn-light">
                     <i className="fas fa-user-circle text-info mr-1"></i> Edit Profile
